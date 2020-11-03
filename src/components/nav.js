ffddbee62cbc3272c706210e870c36cd09e656bf
Adeola Adeyemo <adeola@formpl.us>
2020-11-03 06:41:40 +0100

    AERO-1 | Initialised the next app
    Signed-off-by: Adeola Adeyemo <adeola@formpl.us>

---
+++ src/components/nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+
+const NavWrapper = styled.nav`
+  display: 
+`;
+
+const Nav = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+Nav.propTypes = {
+  
+};
+
+export default Nav;
